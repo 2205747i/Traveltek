11eaac9840feb40c92b4b87a971093b28207dc19
Salman <2194437M@student.gla.ac.uk@>
2017-03-25 11:47:36 +0000

    Starting models.

--- holiday/cruises/models.py
+++ holiday/cruises/models.py
@@ -2,4 +2,11 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-# Create your models here.
+class Cruise(models.Model):
+	code_to_cruise_id = models.CharField(unique = True, null = False, default = None)
+	sail_date = models.DateTimeField(null = False, default = None)
+	sail_nights = models.IntegerField(null = false, default = 0)
+	cruise_id = models.CharField(null = False, default = None)
+	line_id = models.CharField(null = False, default = None)
+	region_id = models.CharField(null = False, default = None)
+	
--- holiday/holiday/settings.py
+++ holiday/holiday/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '(-+u=(gfq9d_shkdgjz)#iwyavgzuwx0w774fo0rai5*%efuyp'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['0.0.0.0:80']
 
 
 # Application definition
