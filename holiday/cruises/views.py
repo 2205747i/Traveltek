b057e0ab162de62be195529eb6893611513cb755
hristo ivanov <hristo ivanov>
2017-03-25 13:16:15 +0000

    views

--- holiday/cruises/views.py
+++ holiday/cruises/views.py
@@ -4,4 +4,12 @@ from django.core.urlresolvers import reverse
 
 # Create your views here.
 def home(request):
+    if request.method == "GET":
+        searchtext = request.GET.get('search_box', None)
+        if searchtext == None:
+            return render(request, 'sesh/search.html', {"none": ""})
+        venues = Venue.objects.filter(name__contains=searchtext)
+        return render(request, 'sesh/search.html', {"venues": venues})
+    else:
+        return render(request, 'sesh/search.html', {"nosearch": ""})
     return render(request, 'cruises/index.html')
