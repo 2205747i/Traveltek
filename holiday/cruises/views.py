afbc46cc82db805c71c156b276ec49ab55973543
hristo ivanov <hristo ivanov>
2017-03-25 13:08:08 +0000

    fixed urls

--- holiday/cruises/views.py
+++ holiday/cruises/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from django.http import HttpResponse, HttpResponseRedirect
+from django.core.urlresolvers import reverse
 
 # Create your views here.
+def home(request):
+    return render(request, 'cruises/index.html')
