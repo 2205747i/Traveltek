641b0ed639696f1b397d1efab7027789949bdd16
Salman <2194437M@student.gla.ac.uk@>
2017-03-25 15:16:09 +0000

    added filtering for cruises

--- holiday/cruises/views.py
+++ holiday/cruises/views.py
@@ -1,14 +1,27 @@
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseRedirect
 from django.core.urlresolvers import reverse
+from django.template.defaultfilters import slugify
+from datetime import datetime
+from cruises.models import Cruise
 
 # Create your views here.
 def home(request):
-
     return render(request, 'cruises/index.html')
 
+def form(request):
+	return render(request, 'cruises/form.html')
+
 def test(request):
-	print(request.data)
-	cruises = Cruise.objects.filter()
-	context_dict["cruises"] = cruises
-	return render(request, results.html, context_dict);
+	print "got it"
+	context_dict = {}
+	if request.method == "POST":
+		start_date = request.POST["start"]
+		start_date = datetime.strptime(start_date, '%Y-%m-%d')
+		end_date = request.POST["end"]
+		end_date = datetime.strptime(end_date, '%Y-%m-%d')
+		cruises = Cruise.objects.filter(sail_date__range = [start_date, end_date])
+		context_dict["cruises"] = cruises
+	# cruises = Cruise.objects.filter()
+	# context_dict["cruises"] = cruises
+	return render(request, 'cruises/results.html', context_dict);
